387d64276c24a2aefdf46bcd1fb7dc090e7d8fd7
Ahmed Essam <96630321+ahmed3ssam@users.noreply.github.com>
2022-05-26 05:37:58 +0200

    Update LevelList.cpp

--- Project1/LevelList.cpp
+++ Project1/LevelList.cpp
@@ -45,28 +45,12 @@ LevelList::LevelList(int MAXLVL, float P)
 	}
 }
 
-LevelList::~LevelList()
-{
-}
-
-int LevelList::len()
-{
-	return this->length;
-}
-
 LevelList::Node* LevelList::createNode( string key, int level)
 {
 	LevelList::Node* n = new Node(key, level);
 	return n;
 }
 
-int LevelList::randomLevel()
-{
-	int lvl = 0;
-	while ((float)rand() / (float)RAND_MAX < P && lvl < MAXLVL - 1)
-		lvl++;
-	return lvl;
-}
 
 string& LevelList::find(int index)
 {
@@ -113,6 +97,130 @@ string& LevelList::operator [](int index)
 	}
 }
 
+
+void LevelList::remove(int index)
+{
+	LevelList::Node::Block* current = head;
+	LevelList::Node::Block** update = new Node::Block * [MAXLVL];
+	LevelList::Node** updateTail = new Node * [MAXLVL];
+	LevelList::Node* currentNode = NULL;
+
+	for (int i = level; i >= 0; i--)
+	{
+		updateTail[i] = head[i].next;
+	}
+
+	assert(index >= 0);
+	assert(this->length >= index);
+
+	for (int i = level; i >= 0; i--)
+	{
+		while (current[i].holds <= index)
+		{
+			index -= current[i].holds;
+			currentNode = current[i].next;
+			current = current[i].next->forward;
+		}
+		updateTail[i] = currentNode;
+		update[i] = current;
+	}
+
+	LevelList::Node* delNode = current->next;
+
+	for (int i = level; i >= 0; i--)
+	{
+		if (update[i][i].next == delNode)
+		{
+			update[i][i].holds += update[i][i].next->forward[i].holds - 1;
+			update[i][i].next = update[i][i].next->forward[i].next;
+		}
+		else
+		{
+			update[i][i].holds -= 1;
+		}
+	}
+
+	for (int i = level; i >= 0; i--)
+	{
+		if (head[i].next == NULL)
+		{
+			level -= 1;
+			tail[i] = NULL;
+		}
+	}
+
+	for (int i = level; i >= 0; i--)
+		if (updateTail[i] != NULL)
+			if (updateTail[i]->forward[i].next == NULL)
+				tail[i] = updateTail[i];
+
+	delete delNode;
+	delete[] update;
+	delete[] updateTail;
+
+	length -= 1;
+	modified = true;
+}
+
+void LevelList::debug()
+{
+	cout << "\n*****Level List*****" << "\n";
+	for (int i = 0; i <= level; i++)
+	{
+		LevelList::Node* node = head[i].next;
+		cout << "Level " << i << ": ";
+		while (node != NULL)
+		{
+			cout << node->data << " ";
+			node = node->forward[i].next;
+		}
+		cout << "\n";
+	}
+	cout << "\ntail\n";
+	for (int i = 0; i <= level; i++)
+	{
+		LevelList::Node* node = tail[i];
+		cout << "Level " << i << ": ";
+		cout.flush();
+		if (node != NULL)
+		{
+			cout << node->data << " ";
+			cout.flush();
+		}
+		cout << "\n";
+	}
+}
+///////////////////////////////////////essam///////////////////////////////////////////
+void LevelList::display()
+{
+	int i = 0;
+	int i_length = 1;
+	int space = std::to_string(length).size();                 
+	LevelList::Node* node = head[0].next;
+	while (node != NULL)
+	{
+		i++;
+		i_length = std::to_string(i).size();
+		for (int k = space; k > i_length; k--)
+		{
+			cout << " ";
+		}
+		cout << i << " " << node->data;
+		node = node->forward[0].next;
+		cout << "\n";
+	}
+}
+int LevelList::randomLevel()
+{
+	int lvl = 0;
+	while ((float)rand() / (float)RAND_MAX < P && lvl < MAXLVL - 1)
+		lvl++;
+	return lvl;
+}
+int LevelList::len()
+{
+	return this->length;
+}
 void LevelList::push(string line)
 {
 	int rlevel = randomLevel();
@@ -208,122 +316,16 @@ void LevelList::insert(int index, string line)
 	length += 1;
 	modified = true;
 }
-
-void LevelList::remove(int index)
-{
-	LevelList::Node::Block* current = head;
-	LevelList::Node::Block** update = new Node::Block * [MAXLVL];
-	LevelList::Node** updateTail = new Node * [MAXLVL];
-	LevelList::Node* currentNode = NULL;
-
-	for (int i = level; i >= 0; i--)
-	{
-		updateTail[i] = head[i].next;
-	}
-
-	assert(index >= 0);
-	assert(this->length >= index);
-
-	for (int i = level; i >= 0; i--)
-	{
-		while (current[i].holds <= index)
-		{
-			index -= current[i].holds;
-			currentNode = current[i].next;
-			current = current[i].next->forward;
-		}
-		updateTail[i] = currentNode;
-		update[i] = current;
-	}
-
-	LevelList::Node* delNode = current->next;
-
-	for (int i = level; i >= 0; i--)
-	{
-		if (update[i][i].next == delNode)
-		{
-			update[i][i].holds += update[i][i].next->forward[i].holds - 1;
-			update[i][i].next = update[i][i].next->forward[i].next;
-		}
-		else
-		{
-			update[i][i].holds -= 1;
-		}
-	}
-
-	for (int i = level; i >= 0; i--)
-	{
-		if (head[i].next == NULL)
-		{
-			level -= 1;
-			tail[i] = NULL;
-		}
-	}
-
-	for (int i = level; i >= 0; i--)
-		if (updateTail[i] != NULL)
-			if (updateTail[i]->forward[i].next == NULL)
-				tail[i] = updateTail[i];
-
-	delete delNode;
-	delete[] update;
-	delete[] updateTail;
-
-	length -= 1;
-	modified = true;
-}
-
-void LevelList::debug()
-{
-	cout << "\n*****Level List*****" << "\n";
-	for (int i = 0; i <= level; i++)
-	{
-		LevelList::Node* node = head[i].next;
-		cout << "Level " << i << ": ";
-		while (node != NULL)
-		{
-			cout << node->data << " ";
-			node = node->forward[i].next;
-		}
-		cout << "\n";
-	}
-	cout << "\ntail\n";
-	for (int i = 0; i <= level; i++)
-	{
-		LevelList::Node* node = tail[i];
-		cout << "Level " << i << ": ";
-		cout.flush();
-		if (node != NULL)
-		{
-			cout << node->data << " ";
-			cout.flush();
-		}
-		cout << "\n";
-	}
-}
-void LevelList::display()
-{
-	int i = 0;
-	int i_length = 1;
-	int space = std::to_string(length).size();                 
-	LevelList::Node* node = head[0].next;
-	while (node != NULL)
-	{
-		i++;
-		i_length = std::to_string(i).size();
-		for (int k = space; k > i_length; k--)
-		{
-			cout << " ";
-		}
-		cout << i << " " << node->data;
-		node = node->forward[0].next;
-		cout << "\n";
-	}
-}
 void LevelList::delete_all()
 {
-	for (int i = 0; i < length; i++)
+	for (int i = length-1; i >= 0; i--)
 	{
-		remove(i);
+		remove(0);
 	}
+}
+LevelList::~LevelList()
+{
+	delete_all();
+	delete[] head;
+	delete[] tail;
 }
